339c9bd6394353686e5d1cea5cabf7a859d3cb41
Dragos Tanta <dragos.tanta@gmail.com>
2024-05-28 08:56:06 +0300

    inital commit

---
+++ src/main/java/page/objects/MenuPage.java
@@ -0,0 +1,24 @@
+package page.objects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class MenuPage {
+
+	public WebDriver driver;
+	
+	public MenuPage(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	//driver.findElement(By.linkText('Login'));
+	public By loginLink = By.linkText("Logins");
+	public By shopLink = By.linkText("BOOKS");
+	
+	public void navigateTo(By locator) {
+		driver.findElement(locator).click();
+	}
+	
+	
+	
+}
